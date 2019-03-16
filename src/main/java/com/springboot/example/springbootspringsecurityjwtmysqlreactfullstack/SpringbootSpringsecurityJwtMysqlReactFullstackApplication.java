244a8a6f0c8df16c9829a33a274c3879c1eb6d4a
JacquelineRP <jacqueline.rodriguez.perez@gmail.com>
2019-03-16 10:35:37 +0000

    Initial commit

---
+++ src/main/java/com/springboot/example/springbootspringsecurityjwtmysqlreactfullstack/SpringbootSpringsecurityJwtMysqlReactFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example.springbootspringsecurityjwtmysqlreactfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringsecurityJwtMysqlReactFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringsecurityJwtMysqlReactFullstackApplication.class, args);
+	}
+
+}
