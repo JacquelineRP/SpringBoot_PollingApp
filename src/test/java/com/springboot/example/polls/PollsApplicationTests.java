0179afae0b965bcd753d2da220bef6bddab276ba
JacquelineRP <jacqueline.rodriguez.perez@gmail.com>
2019-03-17 12:45:40 +0000

    Renaming main packages and classes

--- src/test/java/com/springboot/example/polls/PollsApplicationTests.java
+++ src/test/java/com/springboot/example/polls/PollsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springboot.example.springbootspringsecurityjwtmysqlreactfullstack;
+package com.springboot.example.polls;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootSpringsecurityJwtMysqlReactFullstackApplicationTests {
+public class PollsApplicationTests {
 
 	@Test
 	public void contextLoads() {
